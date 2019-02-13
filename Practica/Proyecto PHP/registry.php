c3e3746453b28b2117a5925eb6357a5dcd2a25c1
claudiomed <cmedrano1490@gmail.com>
2019-02-12 21:29:30 -0600

    Changes to the registry file to verify the input from user

--- Practica/Proyecto PHP/registry.php
+++ Practica/Proyecto PHP/registry.php
@@ -9,14 +9,32 @@
         
     //VALIDATE DATA 
         
-    var_dump(preg_match("/[a-zA-Z]/", $name));
-        
     
-        If(ctype_alpha($nombre) ){
-            echo"Correct name";
+        
+        If(preg_match("/[A-Za-z]/", $nombre) && !preg_match("/[0-9]/", $nombre)){
+            echo"Right name";
         }else{
-            echo"Wrong!";
+            echo"Wrong name!";
         }
+        
+        If(preg_match("/[A-Za-z]/", $apellido) && !preg_match("/[0-9]/", $apellido)){
+            echo"Right last name";
+        }else{
+            echo"Wrong last name!";
+        }
+        
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+           echo"Email entered correctly"; 
+        }else{
+            echo"Incorrect email";
+        }
+       
+        
+        
+        
+        
+       
         
         
      
