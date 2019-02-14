bbd8b7de8051af87e452a0df88bf015d358e1d06
Medrano <cmedrano1490@gmail.com>
2019-02-13 19:25:27 -0600

    Cganges made to show erros in the regisry page. Sidebar and registry files have been modfied.

--- Practica/Proyecto PHP/registry.php
+++ Practica/Proyecto PHP/registry.php
@@ -1,4 +1,6 @@
 <?php
+
+        session_start();
     
     //RETRIEVE DATA FROM FORM 
         
@@ -10,25 +12,67 @@
     //VALIDATE DATA 
         
     
-        
+        //SE VALIDA EL NOMBRE INGRESADO
         If(preg_match("/[A-Za-z]/", $nombre) && !preg_match("/[0-9]/", $nombre)){
-            echo"Right name";
+            $nombre_valido=true;
         }else{
-            echo"Wrong name!";
+            $errores['nombre']="El nombre contiene caracteres no validos";
+            $nombre_valido=false;
         }
         
+        //SE VALIDA EL APELLIDO INGRESADO
         If(preg_match("/[A-Za-z]/", $apellido) && !preg_match("/[0-9]/", $apellido)){
-            echo"Right last name";
+            $apellido_valido=true;
         }else{
-            echo"Wrong last name!";
+            $errores['apellido']="El apellido contiene caracteres no validos";
+            $apellido_valido=false;
         }
         
-        
+        //SE VALIDA EL CORREO ELECTRONICO INGRESADO
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-           echo"Email entered correctly"; 
+            $email_valido=true;
         }else{
-            echo"Incorrect email";
+            $errores['email']="El email contiene caracteres no validos";
+            $email_valido=false;
         }
+        
+        //SE VALIDA LA CONTRASEÑA INGRESADA
+        if(strlen($password)>8 && preg_match("/[A-Z]/", $password) && preg_match("/[0-9]/", $password)){
+            $password_valido=true;
+        }else{
+            if(!preg_match("/[A-Z]/", $password)){
+                $errores['password_no_mayusculas']='El password debe contener al menos una mayuscula </br>';
+                $password_no_mayusculas_valido=false;
+            }
+            if(!preg_match("/[0-9]/", $password)){
+                $errores['password_no_numeros']='El password debe contener al menos un numero</br>';
+                $password_no_mayusculas_valido=false;
+            }
+            if(strlen($password)<8){
+                $errores['password_numero_caracteres']="El password debe contener mas de 8 caracteres </br>";
+                $password_numero_caracteres_valido=false;
+            }
+                   
+        }
+        
+        //SE MUESTRAN LOS ERRORES EN CASO DE HABER
+        /*foreach($errores as $error){
+            echo $error;
+        }*/
+        
+        //SE INGRESAN LOS DATOS A LA BASE DE DATOS EN CASO DE NO HABER ERRORES
+        if(count($errores)==0){
+            
+        }else{
+            $_SESSION['errores']=$errores;
+            header("Location: index.php");
+        }        
+        
+        
+        
+        
+        
+        
        
         
         
